587915cbd65e061356b44432c1d123d876e232f2
Riana Santatra <rianasantatra665@gmail.com>
2025-08-12 13:09:23 +0300

    creation components playing-card, creation html/css playing-card

---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './playing-card.component.html',
+  styleUrl: './playing-card.component.css'
+})
+export class PlayingCardComponent {
+
+}
